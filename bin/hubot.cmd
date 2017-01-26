9cd83fe2f818e791331266560d1fbc227903d21b
Josue Hernandez <josue.hernandez@mercadolibre.cl>
2017-01-26 18:27:23 -0300

    Config Bot with Slack.

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -4,4 +4,4 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "little" %* 
+node_modules\.bin\hubot.cmd --name "Mini" %* 
